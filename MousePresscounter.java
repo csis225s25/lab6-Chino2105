ad756cf34701b73b872d83dcac08d89088fa4891
KolLost <ko16olaw@siena.edu>
2025-03-12 03:50:05 -0400

    ADDED MousePresscounter kola

--- MousePresscounter.java
+++ MousePresscounter.java
@@ -1,13 +1,17 @@
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-public class MousePresscounter extends MouseAdapter implements Runnable{
+public class MousePresscounter extends MouseAdapter implements Runnable,ActionListener {
    
-    int counter = 0;
-   JLabel display = new JLabel("Presses: " + counter);
+    private int counter = 0;
+   private JLabel display = new JLabel("Presses: " + counter);
+   private JPanel panel;
+   private JButton resetButton = new JButton("Reset");
+
 
 
 
@@ -16,46 +20,69 @@ public class MousePresscounter extends MouseAdapter implements Runnable{
 
     @Override
 	public void run() {
-       
-	
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		JFrame frame = new JFrame("MouseDemo");
+		JFrame frame = new JFrame("Mouse Press Counter");
 		frame.setPreferredSize(new Dimension(500, 500));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
 
 		// construct an anonymous class that extends JPanel,
 		// for which we override the paintComponent method
-		JPanel panel = new JPanel() {
+		 panel = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
-
 				super.paintComponent(g);
 
 				FontMetrics fm = g.getFontMetrics();
 
-				
-				int stringWidth = fm.stringWidth(toDisplay);
+				String text = "Mouse Press Count:" + counter;
+				int stringWidth = fm.stringWidth(text);
 				int stringAscent = fm.getAscent();
 
 				int xStart = getWidth() / 2 - stringWidth / 2;
 				int yStart = getHeight() / 2 + stringAscent / 2;
 
-				g.drawString(display, xStart, yStart);
+				g.drawString(text, xStart, yStart);
 			}
 		};
-		frame.add(panel);
+
+		
 		panel.addMouseListener(this);
+		resetButton.addActionListener(this);
+
+		JPanel buttonJPanel = new JPanel();
+		buttonJPanel.add(resetButton);
+		
+
+		frame.add(panel, BorderLayout.CENTER);
+		frame.add(buttonJPanel, BorderLayout.SOUTH);
+
+		
 		
 
 		// display the window we've created
 		frame.pack();
 		frame.setVisible(true);
+
 	}
+
+
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		counter++;
-        toDisplay.setText("Mouse Pressed counter: " + counter);
+        display.setText("Mouse Pressed counter: " + counter);
+		panel.repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e){
+		if (e.getSource() == resetButton) {
+			counter = 0;
+			display.setText("Mouse Pressed counter: " + counter);
+			panel.repaint();
+		}
 	}
 
 	
@@ -68,7 +95,7 @@ public class MousePresscounter extends MouseAdapter implements Runnable{
 
 
 	public static void main(String args[]) {
-		javax.swing.SwingUtilities.invokeLater(new MouseDemo());
+		javax.swing.SwingUtilities.invokeLater(new MousePresscounter());
 	}
 }
 
